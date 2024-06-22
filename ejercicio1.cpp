fe7d2337b56b05e082e62f007b5f4a027c2f03a1
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-22 16:41:21 -0500

    Actualiza ejercicio 1: Se agrega una interfaz con opciones, asimismo se proporciona un bucle para usar el programa varias veces

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int op1,op2,r;
+    int op1,op2,r,salir=0,opc;
     char op;
-    cout<<"Calculadora!"<<endl;
-    cout<<"Introduce los numeros a operar y el operador [numero 1, operador , numero 2 ]: "; cin>>op1>>op>>op2; // introducir 2 operadores y op (la operacion)
-    switch (op){
-    case '+':
-        r=op1+op2;
-        cout<<"La respuesta es: "<<r;
-        break;
-    case '-':
-        r= op1-op2;
-        cout<<"La respuesta es: "<<r;
-        break;
-    case '*':
-        r=op1*op2;
-        cout<<"La respuesta es: "<<r;
-        break;
-    case '/':
-        if(op2!=0){
-            r=op1/op2;
-            cout<<"La respuesta es: "<<r;
-        }else{
-            cout<<"Valor indefinido ( numero 2 = 0 )";
+    do{
+        system("cls");
+        cout<<"Calculadora!"<<endl<<endl;
+        cout<<"1) Operar"<<endl;
+        cout<<"2) Salir"<<endl<<endl;
+        cout<<"Introduce una opcion: "; cin>>opc;
+        system("cls");
+        switch (opc){
+        case 1:
+            cout<<"Introduce los numeros a operar y el operador [numero 1, operador , numero 2 ]: "; cin>>op1>>op>>op2; // introducir 2 operadores y op (la operacion)
+            system("cls");
+            switch (op){
+            case '+':
+                r=op1+op2;
+                cout<<"La respuesta es: "<<r<<endl;
+                system("pause");
+                break;
+            case '-':
+                r= op1-op2;
+                cout<<"La respuesta es: "<<r<<endl;
+                system("pause");
+                break;
+            case '*':
+                r=op1*op2;
+                cout<<"La respuesta es: "<<r<<endl;
+                system("pause");
+                break;
+            case '/':
+                if(op2!=0){
+                    r=op1/op2;
+                    cout<<"La respuesta es: "<<r<<endl;
+                    system("pause");
+                }else{
+                    cout<<"Valor indefinido ( numero 2 = 0 )"<<endl;
+                    system("pause");
+                }
+                break;
+            default:
+                cout<<"Operador desconocido"<<endl;
+                system("pause");
+                break;
+            }
+            break;
+        case 2:
+            salir=1;
+            cout<<"Gracias por usar el programa! "<<endl;
+            system("pause");
+            break;
+        default:
+            cout<<"Valor mal introducido"<<endl;
+            system("pause");
+            break;
         }
-        break;
-    default:
-        cout<<"Valor desconocido";
-        break;
-    }
-
+    } while (salir==0);
+    
     return 0;
 }
